88e1f7d1bfb2eccaac69956d5488ce5baeb4c4f6
Rakesh Mishra <irakeshm.tech@gmail.com>
2019-06-17 01:31:51 +0530

    Adding Log4J Config file and Log File

---
+++ src/main/java/com/amazon/test/utilities/Constants.java
@@ -0,0 +1,13 @@
+
+/* Author: Rakesh Mishra
+ * Class: Constants.Java
+ * Info: Contains the Project Constants 
+*/
+
+package com.amazon.test.utilities;
+
+public class Constants {
+	
+	public static int PAGE_LOAD_TIMEOUT=20;
+	public static int IMPLICIT_WAIT=10;
+}
